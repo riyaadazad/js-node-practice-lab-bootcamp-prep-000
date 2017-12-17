14a72f382c6a99bfd85b20c0a7b5db73d55638a7
Riyaad Azad <azad.riyaad@gmail.com>
2017-12-17 13:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 // write your code below!
 
-var name = "Joe"
-var height = 74
+var name = "Joe";
+var height = 74;
 
-name = "Riyaad"
+name = "Riyaad";
 
-console.log("Name:")
-console.log(name)
+console.log("Name:");
+console.log(name);
 
-console.log("Height:")
-console.log(height + 1)
+console.log("Height:");
+console.log(height + 1);
